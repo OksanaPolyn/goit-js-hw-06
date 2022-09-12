9ac593111dfda776eb572c9235874256c402e85e
OksanaPolyn <Oksanapolynk@gmail.com>
2022-09-12 13:46:24 +0300

    Update 2 @ 8
    Update 2 @ 8

--- js/task-02.js
+++ js/task-02.js
@@ -10,9 +10,9 @@ const ingredients = [
 // Напиши скрипт, который для каждого элемента массива ingredients:
 // Создаст отдельный элемент <li>. Обзательно используй метод document.createElement().
 
-const listIngridient = document.querySelector(`#ingredients`);
-//********OLD SCHOOL - don`t use now!
-//const elements = [];
+// ********OLD SCHOOL - don`t use now!
+// const listIngridient = document.querySelector(`#ingredients`);
+// const elements = [];
 // for (let i = 0 ; i< ingredients.length; i+=1) {
 //   //console.log (ingredients [i]);
 //   const option = ingredients [i];
@@ -25,20 +25,19 @@ const listIngridient = document.querySelector(`#ingredients`);
 //   // console.log(navItemEl);
 //   elements.push (navItemEl)
 // }
-const makeListIngridient = ingredients.map(ingredient => {
-  const option = ingredients [i];
-  //робимо елемент
-  const navItemEl = document.createElement(`li`);
-  // Добавит название ингредиента как его текстовое содержимое. 
-  navItemEl.textContent = `${ingredients[i]}`;
- 
-  navItemEl.classList.add(`item`);  // Добавит элементу класс item.
+
+
+///////////
+const listIngridient = document.querySelector(`#ingredients`); 
+
+const elements = ingredients.map((elem) => {
+ const navItemEl = document.createElement(`li`);
+ navItemEl.textContent = elem;
+ navItemEl.classList.add(`item`);  
   // console.log(navItemEl);
-  elements.push (navItemEl)
+//   elements.push (navItemEl);
+  return navItemEl ;
 });
-
-
 console.log(elements);
-listIngridient.append (...elements)
-
+listIngridient.append(...elements)
 
